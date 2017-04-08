2b7102c245388a5012565fd3334e0dc8a54f08fc
Hitomaru <hunteratthenight@gmail.com>
2017-04-08 11:50:24 +0700

    add search

--- server/sv.js
+++ server/sv.js
@@ -15,7 +15,8 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
