a97ab0e5d8226087c5b0b2945600211a73b20e88
ddawng.quang <ndq.quang511@gmail.com>
2017-04-10 00:19:30 +0700

    search function
    get Id of chosen skill, load all skill to

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,8 +1,56 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MdDialog} from '@angular/material';
+import { APIService } from '../_services/index';
+import {Skill} from '../_models/index';
+import { NgForm } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   templateUrl: './search.component.html',
 })
-export class SearchComponent{}
+export class SearchComponent implements OnInit{
+
+  objectskills : any = {};
+  skills : Skill[] = [];
+  idskill : string;
+  searchOpt : string[]=[];
+
+  constructor(
+    private apiService: APIService,
+    private skillModel : Skill,
+  ){}
+  ngOnInit(){
+    this.apiService.getAllSkills().
+    subscribe(
+    data => {
+      this.objectskills = data.data.docs;
+      for (let i = 0; i < this.objectskills.length; i++){
+        this.skills[i] = this.objectskills[i];
+      }
+
+    },
+    error => {
+        console.log("error")
+    })
+
+  }
+
+  onSubmit(f:NgForm){
+    var value = f.value;
+    console.log (f);
+    console.log (value);
+
+    console.log (value.searchOpt.idskill);
+
+    this.apiService.searchSkill(value.searchOpt.idskill)
+    .subscribe(
+      data => {
+
+      },
+      error => {
+          console.log("error")
+      }
+    )
+
+  }
+}
