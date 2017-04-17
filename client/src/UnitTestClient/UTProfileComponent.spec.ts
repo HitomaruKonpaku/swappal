e1e7af78d1f89958c60fee29e7b7aed09c5fa36e
huutai1995 <nguyenhuutai1995k19t01@gmail.com>
2017-04-17 10:57:32 +0700

    Update Unit Test
    Update Unit Test

--- client/src/UnitTestClient/UTProfileComponent.spec.ts
+++ client/src/UnitTestClient/UTProfileComponent.spec.ts
@@ -51,4 +51,50 @@ describe("Test profile",function(){
         };
         expect(switchForm).toThrow();
     });
+    it('profile.component - onSubmit',function(){
+        var onSubmit = function(value:any){
+            return this.profileService.createProfile(value);
+        }
+        expect(onSubmit).toThrow();
+    });
+    it('profile.component - getProfile',function(){
+        var getProfile = function(email:any){
+            return this.profileService.getProfile(email);
+        }
+        expect(getProfile).toThrow();
+    });
+    it('profile.component - getProfile 2',function(){
+        var getProfile = function(data:any){
+            return this.profile = data.data.profile;
+        }
+        expect(getProfile).toThrow();
+    });
+    it('profile.component - getProfile 3',function(){
+        var getProfile = function(email:any){
+            return this.profileService.getSkills(email);
+        }
+        expect(getProfile).toThrow();
+    });
+    it('profile.component - getProfile 4',function(){
+        var getProfile = function(data:any){
+            return this.skills = data.data.skills;
+        }
+        expect(getProfile).toThrow();
+    });
+    it('profile.component - getProfile 5',function(){
+        var getProfile = function(){
+            for (let i = 0; i < this.skills.have.length;i++){
+                return this.skillHave[i] = this.skills.have[i];
+            }
+        }
+        expect(getProfile).toThrow();
+    });
+    it('profile.component - getProfile 6',function(){
+        var getProfile = function(){
+            for (let i = 0; i <this.skills.want.length;i++){
+                return this.skillWant[i] = this.skills.want[i];
+            }
+        }
+        expect(getProfile).toThrow();
+    });
 });
