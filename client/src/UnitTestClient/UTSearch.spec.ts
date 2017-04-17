37f984707149734191f6adefa97a71e9614239b1
huutai1995 <nguyenhuutai1995k19t01@gmail.com>
2017-04-17 10:37:39 +0700

    Update Unit Test
    Update Unit Test

--- client/src/UnitTestClient/UTSearch.spec.ts
+++ client/src/UnitTestClient/UTSearch.spec.ts
@@ -11,6 +11,20 @@ describe('Test Search',function(){
         };
         expect(ngOnInit).toThrow();
     })
+    it('Search Component - ngOnInit 2',function(){
+        var ngOnInit = function(data:any){
+            return this.objectskills = data.data.docs;
+        };
+        expect(ngOnInit).toThrow();
+    })
+    it('Search Component - ngOnInit 3',function(){
+        var ngOnInit = function(data:any){
+            for (let i = 0; i < this.objectskills.length; i++){
+                return this.skills[i] = this.objectskills[i];
+            }
+        };
+        expect(ngOnInit).toThrow();
+    })
     it('Search Component - onSubmit',function(){
         var onSubmit = function(f:NgForm){
             var value = f.value;
@@ -18,6 +32,54 @@ describe('Test Search',function(){
             console.log (value);
             console.log (value.searchOpt.idskill);
             return this.apiService.searchSkill(value.searchOpt.idskill)
+        }
+        expect(onSubmit).toThrow();
+    })
+    it('Search Component - onSubmit 2',function(){
+        var onSubmit = function(){
+            var str = '{'+ '"'+this.searchOpt+'"' +':["'+this.idskill+'"]}';
+            var json = JSON.parse(str);
+            return this.apiService.searchSkill(json)
+        }
+        expect(onSubmit).toThrow();
+    })
+    it('Search Component - onSubmit 3',function(){
+        var onSubmit = function(data:any){
+            if (data.result.docs.length==0){
+                return this.isShowData = false;
+            }
+        }
+        expect(onSubmit).toThrow();
+    })
+    it('Search Component - onSubmit 4',function(){
+        var onSubmit = function(data:any){
+            return this.isShowData = true;
+        }
+        expect(onSubmit).toThrow();
+    })
+    it('Search Component - onSubmit 5',function(){
+        var onSubmit = function(data:any){
+            for (let i = 0; i<data.result.docs.length;i++){
+                this.resultUser[i] = data.result.docs[i];
+                this.resultUserProfiles[i] = data.result.docs[i].profile;
+                this.resultUserSkill[i] = data.result.docs[i].skills;
+                for( let j =0 ; j< this.resultUserSkill[i].have.length;j++){
+                    return this.skillHave[j] =  this.resultUserSkill[i].have[j];
+                }
+            }
+        }
+        expect(onSubmit).toThrow();
+    })
+    it('Search Component - onSubmit 6',function(){
+        var onSubmit = function(data:any){
+            for (let i = 0; i<data.result.docs.length;i++){
+                this.resultUser[i] = data.result.docs[i];
+                this.resultUserProfiles[i] = data.result.docs[i].profile;
+                this.resultUserSkill[i] = data.result.docs[i].skills;
+                for (let k = 0; k <this.resultUserSkill[i].want.length;k++){
+                    return this.skillWant[k] = this.resultUserSkill[i].want[k];
+                }
+            }
         }
         expect(onSubmit).toThrow();
     })
